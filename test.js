bec8a191d10364c873c91de76d281789cfea9175
SEOYEON HAN <tjdus0824@naver.com>
2022-03-10 12:26:58 +0900

    test.js제출

---
+++ test.js
@@ -0,0 +1,12 @@
+const test= (n)=>{
+    result = [];
+
+    for(let i=1; i<=n; i++){
+        if(i % 2 === 0){
+            result.push(i)
+        };
+    };
+    return result;
+}
+
+const n = 50;
